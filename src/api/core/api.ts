0cadada9e93b0ea700d7438906e2030f7738e98e
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-03-21 00:16:37 +0900

    Feat: api.ts 작성

--- src/api/core/api.ts
+++ src/api/core/api.ts
@@ -1,11 +1,34 @@
 import type { AxiosRequestConfig } from 'axios';
 import axios from 'axios';
+import { getCookie } from '../../utils/cookiefn';
 
+export const token = getCookie('accessToken');
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: 'https://0e7ac26b-1b6c-42c5-bdbe-c19d2b6faabd.mock.pstmn.io',
+  baseURL: 'https://e07b7b82-897d-49ec-96da-07598f327525.mock.pstmn.io/',
   headers: {
     'content-type': 'application/json',
+    Authorization: `Bearer ${token}`,
   },
 };
 
 export const request = axios.create(axiosConfig);
+
+request.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    console.log(error.message);
+    return Promise.reject(error);
+  },
+);
+
+request.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
